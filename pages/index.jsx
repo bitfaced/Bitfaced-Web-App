9f077be6abecc610c5df721813dd844f8ce4a5ff
Phillip Dean <dean@Phillips-MBP.domain>
2019-01-07 16:08:40 -0800

    google anie tix

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import Layout from '../components/Layout';
 import ContentContainer from '../components/content/ContentContainer';
 import { DEFAULT_CONTENT_NODE } from '../constants';
@@ -6,9 +7,13 @@ import { DEFAULT_CONTENT_NODE } from '../constants';
 class Index extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       activeContent: DEFAULT_CONTENT_NODE,
     };
+
+    ReactGA.initialize('UA-83285751-1');
+    ReactGA.pageview('Home Page');
   }
 
   onContentChange = (newContentState) => {
