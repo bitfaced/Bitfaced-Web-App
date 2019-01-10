f8bd2b5b05aec3dd6b5313cbd0e48fa306b388da
Phillip Dean <dean@Phillips-MBP.domain>
2019-01-09 18:35:11 -0800

    contact content ui tweaks

--- components/content/Contact.jsx
+++ components/content/Contact.jsx
@@ -37,11 +37,11 @@ class ContactContent extends React.Component {
           confirmLabel="Back to Home"
         >
           <div>
-            <Heading size={400}>
+            <Heading size={300}>
               If you need to contact Bitfaced, please email eric@bitfaced.com.
             </Heading>
 
-            <Heading size={300} marginTop={40} marginBottom={20}>
+            <Heading size={300} marginTop={20} marginBottom={20}>
               You can message us on Facebook, as well.
             </Heading>
             <iframe title="fb-message" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbitfacedgames%2F&tabs=messages&width=300&height=400&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1629881623992316" width="300" height="400" scrolling="no" frameBorder="0" allowTransparency="true" allow="encrypted-media" />
