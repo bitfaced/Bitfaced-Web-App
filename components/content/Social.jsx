49dc94013c31a51c3b25dfc845837acbfccff9c1
Phillip Dean <dean@Phillips-MacBook-Pro.local>
2019-01-09 22:07:02 -0800

    social content tweaks

--- components/content/Social.jsx
+++ components/content/Social.jsx
@@ -3,8 +3,9 @@ import {
   Dialog,
   Alert,
   Pane,
-  Paragraph,
   Link,
+  Avatar,
+  Heading,
 } from 'evergreen-ui';
 import { PropTypes } from 'prop-types';
 import { DEFAULT_CONTENT_NODE } from '../../constants';
@@ -45,33 +46,76 @@ class SocialContent extends React.Component {
           <div>
             <Pane>
               <Alert
-                intent="none"
-                title="Bitfaced on Facebook"
+                intent="success"
+                hasIcon={false}
                 marginBottom={12}
               >
-                <Paragraph>
-                  <Link href="https://facebook.com/bitfacedgames">Facebook Page</Link>
-                </Paragraph>
-
-                <Link href="https://facebook.com/pages/bitfacedgames">Facebook Group</Link>
+                <Link href="https://facebook.com">
+                  <Avatar
+                    src="https://cdn2.iconfinder.com/data/icons/social-18/512/Facebook-2-512.png"
+                    color="teal"
+                    padding="5px"
+                    name="Facebook"
+                    size={40}
+                  />
+                  <Heading size={400}>
+                    Like our Facebook Page!
+                  </Heading>
+                </Link>
               </Alert>
               <Alert
-                intent="none"
-                title="YouTube"
+                intent="success"
+                hasIcon={false}
                 marginBottom={12}
-              />
-              <Alert
-                intent="none"
-                title="Twitch"
-                marginBottom={12}
-              />
-              <Alert
-                intent="none"
-                title="Subcribe to the podcast"
               >
-  itunes
-  pipaa
-  onemas
+                <Link href="https://facebook.com">
+                  <Avatar
+                    src="https://cdn3.iconfinder.com/data/icons/ultimate-social/150/39_youtube_b-512.png"
+                    color="red"
+                    padding="5px"
+                    name="Youtube"
+                    size={40}
+                  />
+                  <Heading size={400}>
+                    Subcribe to our youtube channel!
+                  </Heading>
+                </Link>
+              </Alert>
+              <Alert
+                intent="success"
+                hasIcon={false}
+                marginBottom={12}
+              >
+                <Link href="https://twitch.com">
+                  <Avatar
+                    src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c540.png"
+                    color="purple"
+                    padding="5px"
+                    name="Twitch"
+                    size={40}
+                  />
+                  <Heading size={400}>
+                    Come watch or game with us on Twitch
+                  </Heading>
+                </Link>
+              </Alert>
+              <Alert
+                intent="success"
+                hasIcon={false}
+                marginBottom={12}
+              >
+                <Avatar
+                  src="https://cdn3.iconfinder.com/data/icons/pyconic-icons-1-2/512/podcast-2-512.png"
+                  color="orange"
+                  padding="5px"
+                  name="Twitch"
+                  size={40}
+                />
+                <Heading size={400}>
+                  Subcribe to the Bitfaced Podcast
+                </Heading>
+                <a href="https://itunes.apple.com/us/podcast/bitfaced/id1002954314?mt=2&app=podcast">Itunes</a>
+                <a href="https://shows.pippa.io/bitfaced">Pippa</a>
               </Alert>
             </Pane>
           </div>
