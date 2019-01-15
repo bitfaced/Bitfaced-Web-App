6fcd2a5e94948710b80d65f953e59592060d8dca
Phillip Dean <dean@Phillips-MBP.domain>
2019-01-14 20:04:23 -0800

    cache busting date to rss feed

--- server/api/bfRssToJson.js
+++ server/api/bfRssToJson.js
@@ -1,8 +1,9 @@
 const Parser = require('rss-parser');
 
 const parser = new Parser();
+const date = new Date();
 
-const rssToJson = () => parser.parseURL('https://feed.pippa.io/public/shows/bitfaced').then((feed) => {
+const rssToJson = () => parser.parseURL(`https://feed.pippa.io/public/shows/bitfaced?t=${date}`).then((feed) => {
   const {
     title,
     content,
