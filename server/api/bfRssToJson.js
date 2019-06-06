5d88aa7e01d1e0e3a516a1a7c6c54384e56626f1
tpendergrass <generoustom@gmail.com>
2019-06-05 19:48:48 -0600

    Switch over to async/await handling.

--- server/api/bfRssToJson.js
+++ server/api/bfRssToJson.js
@@ -3,8 +3,9 @@ const Parser = require('rss-parser');
 const parser = new Parser();
 const date = new Date();
 
-const rssToJson = () => parser.parseURL(`https://feeds.transistor.fm/bitfaced?t=${date}`).then((feed) => {
+async function rssToJson() {
+  const feed = await parser.parseURL(`https://feeds.transistor.fm/bitfaced?t=${date}`);
   return feed;
-});
+}
 
 module.exports = rssToJson;
