c508aafc364094499a7476878d580fabdf0c2aad
tpendergrass <generoustom@gmail.com>
2019-06-09 17:22:20 -0600

    tweak podcastPlayer size and prop passing. adjust logo position (again).

--- components/PodcastPlayer.jsx
+++ components/PodcastPlayer.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 
 // rss feed has share links instead of embed links.
 function swapUrlFromShareToEmbed(url) {
   return url.replace('/s/', '/e/');
 }
 
-const PodcastPlayer = embedUrl => (
+const PodcastPlayer = ({ embedUrl }) => (
   <div>
     <iframe
       title="Bitfaced Podcast Media Player"
-      src={swapUrlFromShareToEmbed(embedUrl.embedUrl)}
+      src={swapUrlFromShareToEmbed(embedUrl)}
       frameBorder="0"
       width="100%"
-      height="220px"
+      height="180px"
       allow="autoplay"
     />
   </div>
 );
 
+PodcastPlayer.propTypes = {
+  embedUrl: PropTypes.string.isRequired,
+};
 export default PodcastPlayer;
