69a1fc90686fb05f4ee65d58a210344c6b57bfeb
tpendergrass <generoustom@gmail.com>
2019-06-10 18:20:44 -0600

    Use localStorage instead of cookie. Waay easier!

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,9 @@ import { Pane } from 'evergreen-ui';
 import { PropTypes } from 'prop-types';
 import PodcastContainer from './PodcastContainer';
 
-const Footer = ({ episodes, lastPlayedEpisode }) => (
+const Footer = ({ episodes }) => (
   <Pane background="tint2" position="absolute" bottom="-4px" width="100%">
-    <PodcastContainer episodes={episodes} lastPlayedEpisode={lastPlayedEpisode} />
+    <PodcastContainer episodes={episodes} />
   </Pane>
 );
 
@@ -14,7 +14,6 @@ Footer.propTypes = {
     title: PropTypes.string,
     link: PropTypes.string,
   })).isRequired,
-  lastPlayedEpisode: PropTypes.string.isRequired,
 };
 
 export default Footer;
