9816469e2e7d9f4575d4cce08c1d34f318e5290a
xtglazex <glaze_tr@yahoo.com>
2020-07-30 11:26:26 -0600

    uploaded FB preview image to s3

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -16,7 +16,7 @@ const meta = ({ latestPodcast }) => {
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://bitfaced.com" />
         <meta property="og:title" content={title} />
-        <meta property="og:image" content="https://scontent.fapa1-1.fna.fbcdn.net/v/t31.0-8/16804133_421665111507243_5942751867111632929_o.jpg?_nc_cat=110&_nc_sid=dd9801&_nc_ohc=5N6DmiLmaMQAX_wKj16&_nc_ht=scontent.fapa1-1.fna&oh=db0b8f3b2569bbbbcb1854707e5ef7a0&oe=5F48DF6D" />
+        <meta property="og:image" content="https://bitfaced-assets.s3.us-east-2.amazonaws.com/FBpre.jpg" />
         <meta property="og:description" content={latestPodcast.content} />
         <meta name="msvalidate.01" content="397DC1B89F47E94AE576233E3E3D298B" />
         <meta name="google-site-verification" content="Skr1DY0RuQrohFqXu5sk0WcMzZp-OVC7inox9ihM0ng" />
